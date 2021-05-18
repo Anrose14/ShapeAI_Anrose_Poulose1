e751d6168debb361f6babcb138eb24f9dc99e2d3
Anrose14 <83343636+Anrose14@users.noreply.github.com>
2021-05-18 19:22:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function red_button(){
+  document.body.style.background="#8B0000"
+}
+
+function blue_button(){
+   document.body.style.background="#191970"
+}
+
+function green_button(){
+   document.body.style.background="#228B22"
+}
